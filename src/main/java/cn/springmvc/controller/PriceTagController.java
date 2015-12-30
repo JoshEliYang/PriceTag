3c34d46fd15dd8b3f5a1bd5460c76a48cfb44c4f
joshy <joshy@gobeta.com.cn>
2015-12-30 15:23:19 +0800

    generate a filter for cors domain

--- src/main/java/cn/springmvc/controller/PriceTagController.java
+++ src/main/java/cn/springmvc/controller/PriceTagController.java
@@ -46,6 +46,10 @@ public class PriceTagController {
 		return HttpUtils.generateResponse("0", "请求成功", tags);
 	}
 	
+	public void getPriceTagsWithAjax() {
+		
+	}
+	
 	/**
 	 * @author Josh Yang
 	 * @description 添加新价签
@@ -134,9 +138,5 @@ public class PriceTagController {
 		}
 		
 		return HttpUtils.generateResponse("0", "请求成功", priceTag);
-	}
-	
-	public void getPriceTagsWithAjax() {
-
 	}
 }

e2d189c4dba622cf7eb227b0fb764318f825307f
joshy <joshy@gobeta.com.cn>
2016-01-04 15:30:19 +0800

    add annotations

--- src/main/java/cn/springmvc/dao/PriceTagDAO.java
+++ src/main/java/cn/springmvc/dao/PriceTagDAO.java
@@ -6,15 +6,21 @@ import cn.springmvc.model.PriceTag;
 
 public interface PriceTagDAO {
 
+	// 添加价签
 	public int insertPriceTag(PriceTag pt);
 	
+	// 获得所有价签
 	public List<PriceTag> selectAllPriceTags();
 	
+	// 删除价签
 	public int deletePriceTag(String id);
 	
+	//更新价签
 	public int updatePriceTag(PriceTag pt);
 	
+	// 查询价签
 	public List<PriceTag> selectPriceTagsByParams(PriceTag pt);
 	
+	// 获得单个价签
 	public PriceTag selectPriceTagsById(String id);
 }

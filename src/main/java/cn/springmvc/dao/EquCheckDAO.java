70c8e017243ddb4e8a5beb8e94be030386ed54dd
joshy <joshy@gobeta.com.cn>
2016-03-10 15:26:32 +0800

    re 3-10 implements all emu check

--- src/main/java/cn/springmvc/dao/EquCheckDAO.java
+++ src/main/java/cn/springmvc/dao/EquCheckDAO.java
@@ -1,5 +1,13 @@
 package cn.springmvc.dao;
 
+import java.util.List;
+
+import cn.springmvc.model.EquCheck;
+
+
 public interface EquCheckDAO {
 
+	public List<EquCheck> getEquCheckListByShopId(String shopId) throws Exception;
+	
+	public int saveOneEquCheck(EquCheck sku) throws Exception;
 }

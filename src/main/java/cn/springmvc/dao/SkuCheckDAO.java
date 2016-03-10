d1dc5dbcd94562936ba9726cb5f76abec4c1e4f0
joshy <joshy@gobeta.com.cn>
2016-03-10 14:53:59 +0800

    re 3-10 implement the sky biz

--- src/main/java/cn/springmvc/dao/SkuCheckDAO.java
+++ src/main/java/cn/springmvc/dao/SkuCheckDAO.java
@@ -1,5 +1,12 @@
 package cn.springmvc.dao;
 
+import java.util.List;
+
+import cn.springmvc.model.SkuCheck;
+
 public interface SkuCheckDAO {
 
+	public List<SkuCheck> getSkuCheckListByShopId(String shopId) throws Exception;
+	
+	public int saveOneSkuCheck(SkuCheck sku) throws Exception;
 }
--- src/main/java/cn/springmvc/service/impl/SkuCheckServiceImpl.java
+++ src/main/java/cn/springmvc/service/impl/SkuCheckServiceImpl.java
@@ -1,9 +1,12 @@
 package cn.springmvc.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.springmvc.dao.SkuCheckDAO;
+import cn.springmvc.model.SkuCheck;
 import cn.springmvc.service.SkuCheckService;
 
 @Service
@@ -11,4 +14,18 @@ public class SkuCheckServiceImpl implements SkuCheckService {
 
 	@Autowired
 	private SkuCheckDAO skuCheckDao;
+
+	public List<SkuCheck> getSkuCheckListByShopId(String shopId)
+			throws Exception {
+		return skuCheckDao.getSkuCheckListByShopId(shopId);
+	}
+
+	public int saveOneSkuCheck(SkuCheck sku) throws Exception {
+		try {
+			skuCheckDao.saveOneSkuCheck(sku);
+		} catch (Exception ex) {
+			return -1;
+		}
+		return 0;
+	}
 }

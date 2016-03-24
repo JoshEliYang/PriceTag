18f2e457ca24bfb2a819d2aea5562727c79c9fc9
zhangqinyang <zhangqy_1988@163.com>
2016-03-24 17:20:46 +0800

    re 3-24-2016 add excel export function

--- src/main/java/cn/springmvc/controller/EquCheckDetailController.java
+++ src/main/java/cn/springmvc/controller/EquCheckDetailController.java
@@ -1,7 +1,12 @@
 package cn.springmvc.controller;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -12,12 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.springmvc.utils.ExcelUtils;
+import com.springmvc.utils.HttpUtils;
+
+import cn.springmvc.model.EquCheck;
 import cn.springmvc.model.EquCheckDetail;
-import cn.springmvc.model.SkuCheckDetail;
 import cn.springmvc.model.difference;
 import cn.springmvc.service.EquCheckDetailService;
-
-import com.springmvc.utils.HttpUtils;
+import cn.springmvc.service.EquCheckService;
 
 
 @Scope("prototype")
@@ -27,6 +35,8 @@ public class EquCheckDetailController {
 
 	@Autowired
 	private EquCheckDetailService equCheckDetailService;
+	@Autowired
+	private EquCheckService equCheckService;
 	
 	/**
 	 * @author Josh Yang
@@ -84,5 +94,114 @@ public class EquCheckDetailController {
 			return HttpUtils.generateResponse("1", "服务器内部错误", null);
 		}
 		return HttpUtils.generateResponse("0", "请求成功", diffList);
+	}
+	
+	/**
+	 * 导出equdetail
+	 * @author victor
+	 * @date 2016-3-24
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/{equCheckId}/excelExport", method = RequestMethod.GET)
+	public void exportSkucheckdetails(@PathVariable String equCheckId, 
+			HttpServletResponse response){
+		PrintWriter writer = null;
+		List<EquCheckDetail> reports = null;
+		JSON json = null;
+		Map<String, Object> responseMap = null;
+		try{
+			writer = response.getWriter();
+			responseMap = new HashMap<String, Object>();
+			
+			reports = equCheckDetailService.getAllDetailsBySkuCheckId(equCheckId);
+			if (reports == null || reports.isEmpty()) {
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json; charset=utf-8");
+				responseMap.put("code", 0);
+				responseMap.put("data", null);
+				responseMap.put("msg", "无数据");
+				json = (JSON) JSON.toJSON(responseMap);
+				writer.println(json.toJSONString());
+				return;
+			}
+			ExcelUtils.listToExcel(reports,
+					this.getLeadToFiledSkuDetails(), "EQU盘点表", response);
+		} catch (Exception e) {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json; charset=utf-8");
+			responseMap.put("code", -1);
+			responseMap.put("data", null);
+			responseMap.put("msg", "服务器内部错误");
+			json = (JSON) JSON.toJSON(responseMap);
+			writer.println(json.toJSONString());
+		}
+	}
+	
+	private LinkedHashMap<String, String> getLeadToFiledSkuDetails() {
+		LinkedHashMap<String, String> superClassMap = new LinkedHashMap<String, String>();
+		superClassMap.put("id", "ID");
+		superClassMap.put("equCheckId", "EQU盘点表编号");
+		superClassMap.put("equName", "设备名称");
+		superClassMap.put("num", "数量");
+
+		return superClassMap;
+	}
+	
+	/**
+	 * 导出equDifference
+	 * @author victor
+	 * @date 2016-3-24
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/{id1}/{id2}/excelExport",method = RequestMethod.GET)
+	public void exportDifference (@PathVariable String id1,@PathVariable String id2, 
+			HttpServletResponse response){
+		PrintWriter writer = null;
+		List<difference> reports = null;
+		JSON json = null;
+		Map<String, Object> responseMap = null;
+		try{
+			writer = response.getWriter();
+			responseMap = new HashMap<String, Object>();
+			
+			reports = equCheckDetailService.getEquDifference(id1, id2);
+			if (reports == null || reports.isEmpty()) {
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json; charset=utf-8");
+				responseMap.put("code", 0);
+				responseMap.put("data", null);
+				responseMap.put("msg", "无数据");
+				json = (JSON) JSON.toJSON(responseMap);
+				writer.println(json.toJSONString());
+				return;
+			}
+			ExcelUtils.listToExcel(reports,
+					this.getLeadToFiledEquDifference(id1,id2), "EQU盘点比较表", response);
+		} catch (Exception e) {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json; charset=utf-8");
+			responseMap.put("code", -1);
+			responseMap.put("data", null);
+			responseMap.put("msg", "服务器内部错误");
+			json = (JSON) JSON.toJSON(responseMap);
+			writer.println(json.toJSONString());
+		}
+	}
+	
+	private LinkedHashMap<String, String> getLeadToFiledEquDifference(String id1,String id2) {
+		LinkedHashMap<String, String> superClassMap = new LinkedHashMap<String, String>();
+		superClassMap.put("name", "EQU盘点表编号");
+		try {
+			EquCheck equ1=equCheckService.getEquCheckById(id1);
+			EquCheck equ2=equCheckService.getEquCheckById(id2);
+			superClassMap.put("param1", equ1.getCreationDate().toString());
+			superClassMap.put("param2", equ2.getCreationDate().toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return superClassMap;
 	}
 }
--- src/main/java/cn/springmvc/controller/SkuCheckDetailController.java
+++ src/main/java/cn/springmvc/controller/SkuCheckDetailController.java
@@ -1,7 +1,12 @@
 package cn.springmvc.controller;
 
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
@@ -12,11 +17,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
+import com.springmvc.utils.ExcelUtils;
 import com.springmvc.utils.HttpUtils;
 
+import cn.springmvc.model.SkuCheck;
 import cn.springmvc.model.SkuCheckDetail;
 import cn.springmvc.model.difference;
 import cn.springmvc.service.SkuCheckDetailService;
+import cn.springmvc.service.SkuCheckService;
 
 
 @Scope("prototype")
@@ -26,6 +35,8 @@ public class SkuCheckDetailController {
 	
 	@Autowired
 	private SkuCheckDetailService skuCheckDetailService;
+	@Autowired
+	private SkuCheckService	skuCheckService;
 	
 	/**
 	 * @author Josh Yang
@@ -79,4 +90,116 @@ public class SkuCheckDetailController {
 		return HttpUtils.generateResponse("0", "请求成功", diffList);
 	}
 	
+	/**
+	 * 导出skudetail
+	 * @author victor
+	 * @date 2016-3-24
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/{skuCheckId}/excelExport", method = RequestMethod.GET)
+	public void exportSkucheckdetails(@PathVariable String skuCheckId, 
+			HttpServletResponse response){
+		PrintWriter writer = null;
+		List<SkuCheckDetail> reports = null;
+		JSON json = null;
+		Map<String, Object> responseMap = null;
+		try{
+			writer = response.getWriter();
+			responseMap = new HashMap<String, Object>();
+			
+			reports = skuCheckDetailService.getAllDetailsBySkuCheckId(skuCheckId);
+			if (reports == null || reports.isEmpty()) {
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json; charset=utf-8");
+				responseMap.put("code", 0);
+				responseMap.put("data", null);
+				responseMap.put("msg", "无数据");
+				json = (JSON) JSON.toJSON(responseMap);
+				writer.println(json.toJSONString());
+				return;
+			}
+			ExcelUtils.listToExcel(reports,
+					this.getLeadToFiledSkuDetails(), "SKU盘点表", response);
+		} catch (Exception e) {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json; charset=utf-8");
+			responseMap.put("code", -1);
+			responseMap.put("data", null);
+			responseMap.put("msg", "服务器内部错误");
+			json = (JSON) JSON.toJSON(responseMap);
+			writer.println(json.toJSONString());
+		}
+	}
+	
+	private LinkedHashMap<String, String> getLeadToFiledSkuDetails() {
+		LinkedHashMap<String, String> superClassMap = new LinkedHashMap<String, String>();
+		superClassMap.put("id", "ID");
+		superClassMap.put("skuCheckId", "SKU盘点表编号");
+		superClassMap.put("goodsNo", "商品编码");
+		superClassMap.put("goodsName", "商品名称");
+		superClassMap.put("num", "数量");
+
+		return superClassMap;
+	}
+	
+	/**
+	 * 导出skuDifference
+	 * @author victor
+	 * @date 2016-3-24
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/{id1}/{id2}/excelExport",method = RequestMethod.GET)
+	public void exportDifference (@PathVariable String id1,@PathVariable String id2, 
+			HttpServletResponse response){
+		PrintWriter writer = null;
+		List<difference> reports = null;
+		JSON json = null;
+		Map<String, Object> responseMap = null;
+		try{
+			writer = response.getWriter();
+			responseMap = new HashMap<String, Object>();
+			
+			reports = skuCheckDetailService.getSkuDifference(id1, id2);
+			if (reports == null || reports.isEmpty()) {
+				response.setCharacterEncoding("UTF-8");
+				response.setContentType("application/json; charset=utf-8");
+				responseMap.put("code", 0);
+				responseMap.put("data", null);
+				responseMap.put("msg", "无数据");
+				json = (JSON) JSON.toJSON(responseMap);
+				writer.println(json.toJSONString());
+				return;
+			}
+			ExcelUtils.listToExcel(reports,
+					this.getLeadToFiledSkuDifference(id1,id2), "SKU盘点比较表", response);
+		} catch (Exception e) {
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json; charset=utf-8");
+			responseMap.put("code", -1);
+			responseMap.put("data", null);
+			responseMap.put("msg", "服务器内部错误");
+			json = (JSON) JSON.toJSON(responseMap);
+			writer.println(json.toJSONString());
+		}
+	}
+	
+	private LinkedHashMap<String, String> getLeadToFiledSkuDifference(String id1,String id2) {
+		LinkedHashMap<String, String> superClassMap = new LinkedHashMap<String, String>();
+		superClassMap.put("name", "SKU盘点表编号");
+		try {
+			SkuCheck sku1=skuCheckService.getSkuCheckById(id1);
+			SkuCheck sku2=skuCheckService.getSkuCheckById(id2);
+			superClassMap.put("param1", sku1.getCreationDate().toString());
+			superClassMap.put("param2", sku2.getCreationDate().toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return superClassMap;
+	}
+	
 }
+
+	

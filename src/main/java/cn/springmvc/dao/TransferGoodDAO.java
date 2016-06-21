21ee333b411df78ce6d5532dee93a4dd284493d3
卢光怡 <9237283413@qq.com>
2016-06-21 17:01:17 +0800

    re 6-21 add TransferGoodDAO and TransferOrderDAO

---
+++ src/main/java/cn/springmvc/dao/TransferGoodDAO.java
@@ -0,0 +1,22 @@
+package cn.springmvc.dao;
+
+import java.util.List;
+
+import cn.springmvc.model.TransferGood;
+
+public interface TransferGoodDAO {
+	
+	public int insertTransferGood(List<TransferGood> transferGoods) throws Exception;
+	
+	public int deleteTransferGoodsByOrderId(int orderId) throws Exception;
+	
+	public int deleteTransferGoodById(int id) throws Exception;
+	
+	public int updateTransferGood(TransferGood transferGood) throws Exception;
+	
+	public List<TransferGood> getTransferGoodsByOrderId(int orderId) throws Exception;
+	
+	public TransferGood getTransferGoodById(int id) throws Exception;
+	
+	
+}
---
+++ src/main/java/cn/springmvc/dao/TransferOrderDAO.java
@@ -0,0 +1,31 @@
+package cn.springmvc.dao;
+
+import java.util.List;
+
+import cn.springmvc.model.TransferOrder;
+
+public interface TransferOrderDAO {
+
+	public int insertTransferOrder(TransferOrder transferOrder) throws Exception;
+	
+	public int deleteTransferOrder(int id) throws Exception;
+
+	public int updateTransferOrder(TransferOrder transferOrder) throws Exception;
+
+	public TransferOrder getTransferOrderById(int id) throws Exception;
+
+	public List<TransferOrder> getTransferOrderListByDealShopId(String dealShopId) throws Exception;
+
+	public List<TransferOrder> getTransferOrderListByApplyShopId(String applyShopId) throws Exception;
+	
+	public List<TransferOrder> getUndealTransferOrderListByDealShopId(String dealShopId) throws Exception;
+	
+	public List<TransferOrder> getDealedTransferOrderListByDealShopId(String dealShopId) throws Exception;
+	
+	public List<TransferOrder> getUndealTransferOrderListByApplyShopId(String applyShopId) throws Exception;
+	
+	public List<TransferOrder> getDealedTransferOrderListByApplyShopId(String applyShopId) throws Exception;
+	
+
+	
+}

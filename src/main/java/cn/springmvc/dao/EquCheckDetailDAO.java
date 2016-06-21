020d38fd8238b59af2ad7386d1d70c532dec4a7a
卢光怡 <9237283413@qq.com>
2016-06-21 17:15:46 +0800

    re 6-21 added two methods (addEquDetailNum and subEquDetailNum)  in order to calculate transfers between a store and the other  store

--- src/main/java/cn/springmvc/dao/EquCheckDetailDAO.java
+++ src/main/java/cn/springmvc/dao/EquCheckDetailDAO.java
@@ -10,4 +10,9 @@ public interface EquCheckDetailDAO {
 	public List<EquCheckDetail> getAllDetailsBySkuCheckId(String skuCheckId) throws Exception;
 	
 	public int insertEquDetail(List<EquCheckDetail> detail) throws Exception;
+	
+	public int addEquDetailNum(String equCheckId , String equName, String num);
+	
+	public int subEquDetailNum(String equCheckId, String equName, String num);
+	
 }
--- src/main/java/cn/springmvc/dao/SkuCheckDetailDAO.java
+++ src/main/java/cn/springmvc/dao/SkuCheckDetailDAO.java
@@ -9,4 +9,8 @@ public interface SkuCheckDetailDAO {
 	public List<SkuCheckDetail> getAllDetailsBySkuCheckId(String skuCheckId) throws Exception;
 	
 	public int insertSkuDetail(List<SkuCheckDetail> detail) throws Exception;
+	
+	public int addSkuDetailNum(String skuCheckId , String goodsNo, String num);
+	
+	public int subSkuDetailNum(String skuCheckId, String goodsNo, String num);
 }
